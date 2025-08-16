1c59aa45446dec00da18f62f7cef6ffca31a67d5
ragnarkor <466023842@qq.com>
2025-08-17 06:11:27 +0800

    重构预约机器人脚本，添加登录过程监控和错误对话框处理功能。优化了搜索可用时间段和预约流程，确保在登录对话框出现时自动重试登录。更新了主程序以支持新的监控机制，提升了用户体验和脚本的稳定性。

--- Smart_Play_Bot/booking_bot.py
+++ Smart_Play_Bot/booking_bot.py
@@ -77,18 +77,33 @@ class BookingBot:
 
             print("Logged in")
 
-    def check_waiting_queue(self):
-        image_xpath = "//img[@src='/static/img/virtual-queue.gif' and @alt='smartplay']"
+    def login_in_process(self, username=None, password=None):
+        """ Input user account info and Login """
 
-        
-        pass
+        username_xpath = "//input[@class='el-input__inner' and @name='pc-login-username']"
+        password_xpath = "//input[@type='password' and @name='pc-login-password']"
+        login_button_xpath = "//div[@data-v-8c95f640='' and @role='button' and contains(text(), '登入')]"
+
+        if username and password:
+            UsernameField = self.driver.find_element(By.XPATH, username_xpath)
+            UsernameField.clear()
+            UsernameField.send_keys(username)
+
+            PasswordField = self.driver.find_element(By.XPATH, password_xpath)
+            PasswordField.clear()
+            PasswordField.send_keys(password)
+
+            time.sleep(0.5)
+
+            LoginButtonElement = self.driver.find_element(By.XPATH, login_button_xpath)
+            self.driver.execute_script("arguments[0].click();", LoginButtonElement)
+
+            print("Logged in")
 
     def search_available_period(self, month:int, day:int, district:str, sport:str):
         """ Redirect to timeslot selection page by given month, day """
 
         month, day = int(month), int(day)
-
-        facility_button_xpath = "/html/body/div/div[1]/div[1]/div/div[1]/div/div[1]/ul/div[1]/li[2]"
 
         sport_input_field_xpath = "/html/body/div/div[2]/div[1]/div[2]/div/div/div/div/div/div[3]/div[1]/div/div[1]"
         input_field_xpath = "/html/body/div/div[2]/div[1]/div[2]/div/div/div/div/div/div[3]/div[1]/div[2]/div[2]/div/div/div[1]/input"
@@ -111,16 +126,8 @@ class BookingBot:
 
         search_button_xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div/div/div/div[3]/div[4]/div"
 
-
-        ### Facility Home Page
-        time.sleep(1)
-        self._wait_located(locater=facility_button_xpath, _type="xpath")
-        FacilityButtonElement = self.driver.find_element(By.XPATH, facility_button_xpath)
-        self.driver.execute_script("arguments[0].click();", FacilityButtonElement)
-
-
         ### Select Sport Type
-        time.sleep(2)
+        time.sleep(1)
         self._wait_located(locater=sport_input_field_xpath, _type="xpath")
         SportInputFieldElement = self.driver.find_element(By.XPATH, sport_input_field_xpath)
         SportInputFieldElement.click()
@@ -133,8 +140,6 @@ class BookingBot:
         SportTypeElement = self.driver.find_element(By.XPATH, sport_type_xpath)
         SportTypeElement.click()
 
-        print("Select Sport Type")
-
 
         ### Select District
         time.sleep(1)
@@ -146,27 +151,27 @@ class BookingBot:
         DistrictElement = self.driver.find_element(By.XPATH, district_xpath)
         self.driver.execute_script("arguments[0].click();", DistrictElement)
 
-        print("Select District")
-
-
         ### Select Date
-        time.sleep(1)
-        self._wait_located(locater=date_input_field_xpath, _type="xpath")
-        DateInputFieldElement = self.driver.find_element(By.XPATH, date_input_field_xpath)
-        self.driver.execute_script("arguments[0].click();", DateInputFieldElement)
+        try:
+            time.sleep(1)
+            self._wait_located(locater=date_input_field_xpath, _type="xpath")
+            DateInputFieldElement = self.driver.find_element(By.XPATH, date_input_field_xpath)
+            self.driver.execute_script("arguments[0].click();", DateInputFieldElement)
 
-        self._wait_located(locater=date_xpath, _type="xpath")
-        DateElement = self.driver.find_element(By.XPATH, date_xpath)
-        self.driver.execute_script("arguments[0].click();", DateElement)
+            self._wait_located(locater=date_xpath, _type="xpath")
+            DateElement = self.driver.find_element(By.XPATH, date_xpath)
+            self.driver.execute_script("arguments[0].click();", DateElement)
 
-        print("Select Date")
+        except Exception as e:
+            return False
+
 
         ### Select Search Button
         SearchButtonElement = self.driver.find_element(By.XPATH, search_button_xpath)
         self.driver.execute_script("arguments[0].click();", SearchButtonElement)
         
         print("Click Search Button")
-
+        return True
 
     def _classify_time(self, timeslot_str:str):
         """ Categorize timeslot_str into [Morning, Afternoon, Night] for redirection"""
@@ -219,7 +224,6 @@ class BookingBot:
         self.driver.execute_script("arguments[0].click();", TimeslotElement)
         print(f"Click {timeslot_str}")
 
-
     def _clear_selected_sessions(self):
         """ Clear selected session states """
         time.sleep(1)
@@ -230,27 +234,25 @@ class BookingBot:
         try:
             self._wait_located(locater=session_elements_xpath, _type="xpath")
         except:
-            print("Clear all selected elements")
+            print("No selected element")
             return
         
         # Get all matching elements
         session_elements = self.driver.find_elements(By.XPATH, session_elements_xpath)
         
         if session_elements:
-            print(f"Found {len(session_elements)} session-tag-box-select elements")
-            
             # Click each element
             for i, element in enumerate(session_elements):
                 self.driver.execute_script("arguments[0].click();", element)
-                print(f"Clicked element {i+1}")
                 time.sleep(0.5)
         else:
             print("Clear all selected elements")
             return
 
-
     def check_timeslot_availability(self, timeslot_list:list):
         """ Check the timeslot availability """
+
+        continue_button_xpath = "//div[@data-v-8c95f640 and @class='xp-button xp-primary-d']//div[@tabindex='0' and @role='button']"
 
         self._clear_selected_sessions()
 
@@ -286,8 +288,15 @@ class BookingBot:
                     sibling_element = self.driver.find_element(By.XPATH, sibling_xpath)
                     self.driver.execute_script("arguments[0].click();", sibling_element)
                     time.sleep(0.5)
-                
+
+                # Click continue button
+                time.sleep(1)
+                self._wait_located(locater=continue_button_xpath, _type="xpath")
+                ContinueButtonElement = self.driver.find_element(By.XPATH, continue_button_xpath)
+                self.driver.execute_script("arguments[0].click();", ContinueButtonElement)
                 print(f"Available group: {current_timeslot_group}")
+
+                time.sleep(1)
                 break
             else:
                 i += 1
@@ -297,10 +306,8 @@ class BookingBot:
         
         return True
 
-
     def wait_for_booking(self):
         """ Wait for booking """
-        continue_button_xpath = "//div[@data-v-8c95f640 and @class='xp-button xp-primary-d']//div[@tabindex='0' and @role='button']"
         cancel_button_xpath = "//div[@class='dialog-box' and @role='dialog']/div[@class='btn-box']/div[@class='cancel-button' and @role='button']"
         continue2_button_xpath = "/html/body/div/div[2]/div[4]/div/div/div/div[2]/div/div[2]/div[2]/div"
 
@@ -309,10 +316,6 @@ class BookingBot:
         confirm_button_xpath = "/html/body/div/div[2]/div[3]/div/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div"
         confirm_payment_xpath = "/html/body/div/div[2]/div[3]/div/div/div/div[3]/div[2]/div"
 
-        time.sleep(1)
-        self._wait_located(locater=continue_button_xpath, _type="xpath")
-        ContinueButtonElement = self.driver.find_element(By.XPATH, continue_button_xpath)
-        self.driver.execute_script("arguments[0].click();", ContinueButtonElement)
 
         time.sleep(1)
         self._wait_located(locater=cancel_button_xpath, _type="xpath")
@@ -417,107 +420,41 @@ class BookingBot:
         print("Click Payment Button")
         return True
 
-    def payment(self, cardholder:str, card_num:str, card_month:str, card_year:str, security_code:str):
-        """ Select payment method and payment processing """
+    def monitor_dialog(self):
+        """Monitor if the error dialog or login dialog is present, and handle accordingly."""
+        error_dialog_xpath = "//div[@role='dialog' and @id='dialog' and .//div[@class='button' and contains(text(), '關閉')]]"
+        close_btn_xpath = ".//div[@class='button' and contains(text(), '關閉')]"
+        login_dialog_xpath = "//div[@role='dialog' and @class='el-dialog' and .//*[contains(text(), '登入')]]"
+        
+        try:
+            ErrorDialogElement = self.driver.find_element(By.XPATH, error_dialog_xpath)
+            if ErrorDialogElement.is_displayed():
+                CloseBtnElement = ErrorDialogElement.find_element(By.XPATH, close_btn_xpath)
+                self.driver.execute_script("arguments[0].click();", CloseBtnElement)
+                print("Closed error dialog")
+                return True
+        except Exception:
+            pass
 
-        mastercard_payment_xpath = "/html/body/div/div[2]/div[2]/div/div/div/div[2]/div[7]/div[2]/img"
-        confirm_payment_xpath = "/html/body/div/div[2]/div[2]/div/div/div/div[4]/div[2]/div"
+        # Then, check for the login dialog
+        try:
+            LoginDialogElement = self.driver.find_element(By.XPATH, login_dialog_xpath)
+            if LoginDialogElement.is_displayed():
+                return True
+        except Exception:
+            pass
 
-        iframe1_xpath = "/html/body/app-root/app-payment-detail-form/div/div/div/div/div[1]/div[2]/form/div[1]/app-payment-method-card-layout/div[2]/app-payment-method-card/div/div/div/div[1]/div/div[1]/div/iframe"
-        iframe2_xpath = "/html/body/app-root/app-payment-detail-form/div/div/div/div/div[1]/div[2]/form/div[1]/app-payment-method-card-layout/div[2]/app-payment-method-card/div/div/div/div[2]/div/div[1]/div[1]/iframe"
-        iframe3_xpath = "/html/body/app-root/app-payment-detail-form/div/div/div/div/div[1]/div[2]/form/div[1]/app-payment-method-card-layout/div[2]/app-payment-method-card/div/div/div/div[3]/div[1]/div[1]/div/span[1]/div/iframe"
-        iframe4_xpath = "/html/body/app-root/app-payment-detail-form/div/div/div/div/div[1]/div[2]/form/div[1]/app-payment-method-card-layout/div[2]/app-payment-method-card/div/div/div/div[3]/div[1]/div[1]/div/span[2]/div/iframe"
-        iframe5_xpath = "/html/body/app-root/app-payment-detail-form/div/div/div/div/div[1]/div[2]/form/div[1]/app-payment-method-card-layout/div[2]/app-payment-method-card/div/div/div/div[3]/div[2]/div[1]/div[1]/iframe"
+        return False
 
-        payment_button_xpath = "/html/body/app-root/app-payment-detail-form/div/div/div/div/div[1]/div[2]/form/div[2]/div/app-pay-button/app-button/div/button"
-
-        # Mastercard payment
-        time.sleep(1)
-        self._wait_located(locater=mastercard_payment_xpath, _type="xpath")
-        MastercardPaymentButtonElement = self.driver.find_element(By.XPATH, mastercard_payment_xpath)
-        MastercardPaymentButtonElement.click()
-        print("Select Mastercard")
-
-
-        time.sleep(1)
-        self._wait_located(locater=confirm_payment_xpath, _type="xpath")
-        ConfirmPaymentButtonElement = self.driver.find_element(By.XPATH, confirm_payment_xpath)
-        self.driver.execute_script("arguments[0].click();", ConfirmPaymentButtonElement)
-
-
-        # Switch to the new payment tab
-        time.sleep(10)
-        self.driver.switch_to.window(self.driver.window_handles[-1])
-        time.sleep(1)
-
-        print("input credit card info")
-
-
-        # Card holder field
-        iframe1_element = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, iframe1_xpath)))
-
-        self.driver.switch_to.frame(iframe1_element) # Switch to the iframe
-
-        cardholder_xpath = "//input[@type='text' and @id='nameOnCard']"
-        self._wait_located(locater=cardholder_xpath, _type="xpath")
-        self.driver.find_element(By.XPATH, cardholder_xpath).send_keys(cardholder)
-        print("Input cardholder")
-
-        self.driver.switch_to.default_content() # Switch back
-
-
-        # Card number field
-        iframe2_element = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, iframe2_xpath)))
-
-        self.driver.switch_to.frame(iframe2_element) # Switch to the iframe
-
-        card_number_xpath = "//input[@type='tel' and @id='number']"
-        self.driver.find_element(By.XPATH, card_number_xpath).send_keys(card_num)
-        print("Input card number")
-
-        self.driver.switch_to.default_content()
-
-
-        # Card expiry month
-        iframe3_xpath = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, iframe3_xpath)))
-
-        self.driver.switch_to.frame(iframe3_xpath) # Switch to the iframe
-
-        expiry_month_xpath = "//input[@type='tel' and @id='expiryMonth']"
-        self.driver.find_element(By.XPATH, expiry_month_xpath).send_keys(card_month)
-        print("Input expiry month")
-
-        self.driver.switch_to.default_content()
-
-
-        # Card expiry year
-        iframe4_xpath = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, iframe4_xpath)))
-
-        self.driver.switch_to.frame(iframe4_xpath) # Switch to the iframe
-
-        expiry_year_xpath = "//input[@type='tel' and @id='expiryYear']"
-        self.driver.find_element(By.XPATH, expiry_year_xpath).send_keys(card_year)
-        print("Input expiry year")
-
-        self.driver.switch_to.default_content()
-
-
-        # Card security code
-        iframe5_xpath = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, iframe5_xpath)))
-
-        self.driver.switch_to.frame(iframe5_xpath) # Switch to the iframe
-
-        security_code_xpath = "//input[@type='tel' and @id='securityCode']"
-        self.driver.find_element(By.XPATH, security_code_xpath).send_keys(security_code)
-        print("Input security code")
-
-        self.driver.switch_to.default_content()
-
-
-        # Payment button
-        time.sleep(1)
-        self._wait_located(locater=payment_button_xpath, _type="xpath")
-        PaymentButtonElement = self.driver.find_element(By.XPATH, payment_button_xpath)
-        #self.driver.execute_script("arguments[0].click();", PaymentButtonElement)
-
-        print("Click Payment Button")
+    def run_with_login_monitor(self, func, *args, username=None, password=None, **kwargs):
+        """Continuously monitor login dialog and retry the function until it succeeds. If the function raises an exception, it will be propagated to the outer layer."""
+        while True:
+            if self.monitor_dialog():
+                self.login_in_process(username, password)
+            
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception:
+                time.sleep(1)
+                continue
--- Smart_Play_Bot/smart_play_bot.py
+++ Smart_Play_Bot/smart_play_bot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-import os
 
 # Set UTF-8 encoding for Windows console
 if sys.platform.startswith('win'):
@@ -29,7 +28,7 @@ def main(
     keep_alive:bool = False
 ):
 
-    url = "https://www.smartplay.lcsd.gov.hk/website/tc/index.html"
+    url = "https://www.smartplay.lcsd.gov.hk/facilities/home"
 
     options = Options()
     if options_list:
@@ -41,6 +40,7 @@ def main(
     
     driver = webdriver.Chrome(options=options)
     driver.get(url)
+    driver.maximize_window()
 
     config = extract_config("config.yml")
     username = config["username"]
@@ -58,37 +58,41 @@ def main(
 
 
     bot = BookingBot(driver)
-    bot.get_login_page()
-    bot.login(username, password)
-    bot.search_available_period(booking_month, booking_day, district, sport)
 
-    # driver.get("https://www.smartplay.lcsd.gov.hk/facilities/search-result?district=KC&startDate=2025-08-22&typeCode=BASC&venueCode=&sportCode=BAGM&typeName=%E7%B1%83%E7%90%83&frmFilterType=&venueSportCode=&isFree=false")
-
-    bot.select_timeslot(timeslot, venue, sport_item)
-    
-    # Poll for available timeslots
+    # Poll for available period
     while True:
-        if bot.check_timeslot_availability(timeslot_list):
-            print("Available timeslots found, proceeding to booking")
+        if bot.search_available_period(booking_month, booking_day, district, sport):
             break
         else:
-            print("No available timeslots, waiting 1 minute and refreshing...")
-            time.sleep(60)
+            print("No available date, waiting 5s and refreshing...")
+            time.sleep(5)
             driver.refresh()
             time.sleep(2)  # Wait for page to load
-    
-    bot.wait_for_booking()
 
-    payment_success = bot.pps_payment(pps_card_num, pps_card_pwd)
-    
-    if payment_success:
-        print("Payment process completed successfully!")
-        # Set exit code to indicate success
-        sys.exit(0)
-    else:
-        print("Payment process failed!")
-        # Set exit code to indicate failure
-        sys.exit(1)
+    # driver.get("https://www.smartplay.lcsd.gov.hk/facilities/select/court?venueId=233&fatId=505&venueName=%E5%9C%9F%E7%93%9C%E7%81%A3%E9%AB%94%E8%82%B2%E9%A4%A8&sessionIndex=2&dateIndex=2&playDate=2025-08-22&district=KC&typeCode=BASC&sportCode=BAGM&frmFilterType=&isFree=false")
+
+    bot.select_timeslot(timeslot, venue, sport_item)
+
+    bot.run_with_login_monitor(bot.check_timeslot_availability,timeslot_list, username=username, password=password)
+    # Poll for available timeslots
+    while True:
+        if bot.run_with_login_monitor(bot.check_timeslot_availability,timeslot_list, username=username, password=password):
+            break
+        else:
+            print("No available timeslots, waiting 5s and refreshing...")
+            time.sleep(5)
+            driver.refresh()
+            time.sleep(2)  # Wait for page to load
+
+    bot.run_with_login_monitor(bot.wait_for_booking, username=username, password=password)
+    # payment_success = bot.pps_payment(pps_card_num, pps_card_pwd)
+
+    # if payment_success:
+    #     print("Payment process completed successfully!")
+    #     sys.exit(0) # success
+    # else:
+    #     print("Payment process failed!")
+    #     sys.exit(1) # failure
 
 
 if __name__ == "__main__":
